2f8e5c739d3abffe28b6f13c786b9747a5265491
richmandlx <jannupet@googlemail.com>
2022-07-09 14:30:04 +0200

    t init

--- terra.tf
+++ terra.tf
@@ -23,10 +23,10 @@ resource "azurerm_container_registry" "acr" {
   name                = "containerRegistryRenate1"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Premium"
+  sku                 = "Basic"
   admin_enabled       = false
   georeplications {
-    location                = "East US"
+    location                = "westeurope"
     zone_redundancy_enabled = true
     tags                    = {}
   }
