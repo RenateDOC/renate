90141f994344df12257c63b90478dee8892a4e2d
richmandlx <jannupet@googlemail.com>
2022-07-09 14:20:52 +0200

    t init

--- terra.tf
+++ terra.tf
@@ -18,3 +18,21 @@ resource "azurerm_resource_group" "rg" {
   name     = "renate2"
   location = "westus2"
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = "containerRegistryRenate1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+  admin_enabled       = false
+  georeplications {
+    location                = "East US"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+  georeplications {
+    location                = "westeurope"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+}
